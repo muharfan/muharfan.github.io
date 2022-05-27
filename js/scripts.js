27c74dd91aa85ba102804676234e561058b9cd81
muharfan <92298398+muharfan@users.noreply.github.com>
2022-05-27 16:37:54 +0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,62 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('skillDisplay', () => ({
-        skills: [{
-                'title': 'Photoshop',
-                'percent': '60',
-            },
+dokumen . addEventListener ( 'alpine:init' ,  ( )  =>  {
+    Alpen . data ( 'skillDisplay' ,  ( )  =>  ( {
+        keterampilan : [ {
+                'judul' : 'Photoshop' ,
+                'persen' : '50' ,
+            } ,
             {
-                'title': 'Mc.Office',
-                'percent': '50',
-            },
+                'title' : 'Mc.Excel' ,
+                'persen' : '50' ,
+            } ,
             {
-                'title': 'PHP',
-                'percent': '45',
-            },
+                'judul' : 'Mc.Word' ,
+                'persen' : '50' ,
+            } ,
             {
-                'title': 'CSS',
-                'percent': '70',
-            },
+                'judul' : 'Mc.Power Point' ,
+                'persen' : '70' ,
+            } ,
             {
-                'title': 'HTML',
-                'percent': '70',
-            },
+                'judul' : 'HTML' ,
+                'persen' : '50' ,
+            } ,
             {
-                'title': 'Javascript',
-                'percent': '30',
-            },
-        ],
-        currentSkill: {
-            'title': 'None',
-            'percent': '0',
+                'judul' : 'CSS' ,
+                'persen' : '50' ,
+            } ,
+        ] ,
+        keterampilan saat ini : {
+            'judul' : 'Tidak ada' ,
+            'persen' : '0' ,
         }
-    }));
-});
+    } ) ) ;
+} ) ;
 
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyzHXPpLgDaZqIb_4QrX_q9-aRn4Aygn2xMXn7wrs_GkigrnblDG3xVFYeCtmXHooJsxQ/exec';
-         const form = document.forms['submit-contactus']
-         const btnSend = document.querySelector('.btn-send');
-         const btnLoading = document.querySelector('.btn-loading');
-         const myAlert = document.querySelector('.my-alert');
+const  scriptURL  =  'https://script.google.com/macros/s/AKfycbyzHXPpLgDaZqIb_4QrX_q9-aRn4Aygn2xMXn7wrs_GkigrnblDG3xVFYeCtmXHooJsxQ/exec' ;
+          bentuk  const =  dokumen . formulir [ 'kirim-kontak' ]
+         const  btnSend  =  dokumen . querySelector ( '.btn-send' ) ;
+         const  btnLoading  =  dokumen . querySelector ( '.btn-loading' ) ;
+         const  myAlert  =  dokumen . querySelector ( '.my-alert' ) ;
 
-         form.addEventListener('submit', e => {
-             e.preventDefault()
-             //submit click
-             btnLoading.classList.toggle('d-none');
-             btnSend.classList.toggle('d-none');
-             fetch(scriptURL, {
-                     method: 'POST',
-                     body: new FormData(form)
-                 })
-                 .then(response => {
-                     //tampilkan tombol send
-                     btnLoading.classList.toggle('d-none');
-                     btnSend.classList.toggle('d-none');
-                     //tampilkan alert
-                     myAlert.classList.toggle('d-none')
-                     //reset form
-                     form.reset();
-                     console.log('Success!', response)
-                 })
-                 .catch(error => console.error('Error!', error.message))
-         })
+         bentuk . addEventListener ( 'kirim' ,  e  =>  {
+             e . mencegahDefault ( )
+             //kirim klik
+             btnMemuat . daftar kelas . toggle ( 'tidak ada' ) ;
+             btnKirim . daftar kelas . toggle ( 'tidak ada' ) ;
+             ambil ( scriptURL , { 
+                     metode : 'POST' ,
+                     badan : FormData baru  ( formulir )
+                 } )
+                 . lalu ( respon  =>  {
+                     //tampilkan tombol kirim
+                     btnMemuat . daftar kelas . toggle ( 'tidak ada' ) ;
+                     btnKirim . daftar kelas . toggle ( 'tidak ada' ) ;
+                     //tampilkan peringatan
+                     peringatan saya . daftar kelas . beralih ( 'tidak ada' )
+                     //setel ulang formulir
+                     bentuk . ulang ( ) ;
+                     konsol . log ( 'Berhasil!' ,  tanggapan )
+                 } )
+                 . catch ( error  =>  console . error ( 'Error!' ,  error . message ) )
+         } )
